017ac6e165400a5bfca7e7d46fbe57c5924aad5f
Yurok <yurok.kolesnikov@gmail.com>
2019-12-15 20:00:22 +0500

    fixed

--- app/src/main/java/com/yur0k/homeworkfive/MainActivity.java
+++ app/src/main/java/com/yur0k/homeworkfive/MainActivity.java
@@ -3,11 +3,14 @@ package com.yur0k.homeworkfive;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewCityName;
+    private Button buttonOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,18 @@ public class MainActivity extends AppCompatActivity {
         textViewCityName = findViewById(R.id.textViewCityNameNow);
         textViewCityName.setText("Moscow");
 
+        buttonOk = findViewById(R.id.buttonOk);
+        buttonOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateWeather(textViewCityName.getText().toString());
+            }
+        });
+
     }
+
+    private void updateWeather(String cityName){
+        
+    }
+
 }
