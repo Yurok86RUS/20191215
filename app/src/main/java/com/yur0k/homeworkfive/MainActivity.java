42ffd4c6e3a59dc4ec9bffd2640822e2d4bb2699
Yurok <yurok.kolesnikov@gmail.com>
2019-12-15 19:42:44 +0500

    add gson & model

--- app/src/main/java/com/yur0k/homeworkfive/MainActivity.java
+++ app/src/main/java/com/yur0k/homeworkfive/MainActivity.java
@@ -3,12 +3,19 @@ package com.yur0k.homeworkfive;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewCityName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewCityName = findViewById(R.id.textViewCityNameNow);
+        textViewCityName.setText("Moscow");
+
     }
 }
